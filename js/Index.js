4486047a0606a530cf01f027314704d1cc6ddfc0
Arthur <arthurakendenguesonnet@gmail.com>
2024-02-06 07:02:05 +0100

    Dépot 002
    avec évolution du code javascript.

--- js/Index.js
+++ js/Index.js
@@ -8,6 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
   let btnMultiplication = document.querySelector("#multiplication");
   let btnSoustraction = document.querySelector("#soustraction");
   let btnPlus = document.querySelector("#plus");
+  let btnPoint = document.querySelector("#point");
+  let affichageOperation = document.querySelector("#calculencours");
+  let btnEqual = document.querySelector("#equal");
 
   let btnZero = document.querySelector("#zero");
   let btnOne = document.querySelector("#one");
@@ -22,78 +25,89 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Fonction pour gérer le clic sur les boutons numériques
   function handleButtonClick(value) {
-    zonesSaisie.value = value;
-  }
-
-  function handleButtonClick(value) {
-    if (zonesSaisie.value === "0") {
-      zonesSaisie.value = value;
-    } else {
-      zonesSaisie.value + value;
-    }
-    // Si la valeur actuelle est "0", remplacez-la par la nouvelle valeur
-    // Sinon, ajoutez la nouvelle valeur à la fin de la valeur existante
     zonesSaisie.value =
       zonesSaisie.value === "0" ? value : zonesSaisie.value + value;
   }
 
-  btnPlus.addEventListener("click", function () {
-    zonesSaisie.value = "+";
-  });
-
-  btnSoustraction.addEventListener("click", function () {
-    zonesSaisie.value = "-";
-  });
-
-  btnDivision.addEventListener("click", function () {
-    zonesSaisie.value = "/";
-  });
-
-  btnMultiplication.addEventListener("click", function () {
-    zonesSaisie.value = "*";
-  });
-
+  // Gestion des événements pour les boutons numériques
   btnZero.addEventListener("click", function () {
     handleButtonClick("0");
+    affichageOperation.innerHTML += "0";
   });
 
   btnOne.addEventListener("click", function () {
     handleButtonClick("1");
+    affichageOperation.innerHTML += "1";
   });
 
   btnTwo.addEventListener("click", function () {
     handleButtonClick("2");
+    affichageOperation.innerHTML += "2";
   });
 
   btnThree.addEventListener("click", function () {
     handleButtonClick("3");
+    affichageOperation.innerHTML += "3";
   });
 
   btnFour.addEventListener("click", function () {
     handleButtonClick("4");
+    affichageOperation.innerHTML += "4";
   });
 
   btnFive.addEventListener("click", function () {
     handleButtonClick("5");
+    affichageOperation.innerHTML += "5";
   });
 
   btnSix.addEventListener("click", function () {
     handleButtonClick("6");
+    affichageOperation.innerHTML += "6";
   });
 
   btnSeven.addEventListener("click", function () {
     handleButtonClick("7");
+    affichageOperation.innerHTML += "7";
   });
 
   btnHeight.addEventListener("click", function () {
     handleButtonClick("8");
+    affichageOperation.innerHTML += "8";
   });
 
   btnNine.addEventListener("click", function () {
     handleButtonClick("9");
+    affichageOperation.innerHTML += "9";
   });
 
-  zonesSaisie.value = +value;
-});
+  btnPoint.addEventListener("click", function () {
+    zonesSaisie.value += ",";
+    affichageOperation.innerHTML += ",";
+  });
 
-/* JavaScript */
+  btnPlus.addEventListener("click", function () {
+    zonesSaisie.value += "+";
+    affichageOperation.innerHTML += "+";
+  });
+
+  btnSoustraction.addEventListener("click", function () {
+    zonesSaisie.value += "-";
+    affichageOperation.innerHTML += "-";
+  });
+
+  btnDivision.addEventListener("click", function () {
+    zonesSaisie.value += "/";
+    affichageOperation.innerHTML += "/";
+  });
+
+  btnMultiplication.addEventListener("click", function () {
+    zonesSaisie.value += "*";
+    affichageOperation.innerHTML += "*";
+  });
+
+  btnEqual.addEventListener("click", function () {
+    somme = eval(zonesSaisie.value);
+    zonesSaisie.value = somme;
+    affichageOperation.innerHTML = zonesSaisie.value;
+  });
+});
